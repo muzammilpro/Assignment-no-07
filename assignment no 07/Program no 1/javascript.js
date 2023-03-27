f0c2671ba04ba759e7c963eae8bf8fabb99e1e87
muzammilpro <muzammilhusnain7@gmail.com>
2023-03-27 23:52:48 +0500

    JavaScript Programs

---
+++ assignment no 07/Program no 1/javascript.js
@@ -0,0 +1,14 @@
+// Write a js program to find maximum between two numbers.
+
+var enterFirstNum =+ prompt("Enter First Number:");
+var enterSecondNumber  =+ prompt("Enter Second Number:");
+
+
+if ( enterFirstNum > enterSecondNumber ){
+
+    console.log("First Number is Greater");
+}
+
+else{
+     console.log("Second Number is Greater");
+}
