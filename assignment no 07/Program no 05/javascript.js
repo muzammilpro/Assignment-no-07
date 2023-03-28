3b399f1f291a1eb7357013d7284ac0c5423a0dc5
muzammilpro <muzammilhusnain7@gmail.com>
2023-03-28 19:38:42 +0500

    new program added

---
+++ assignment no 07/Program no 05/javascript.js
@@ -0,0 +1,10 @@
+//Write a js program to check whether a number is even or odd.
+
+var num =+ prompt("Enter a number:");
+
+if(num % 2 == 0){
+    console.log("It is an even number.");
+}
+else{
+    console.log("It is an odd number.");
+}
