2376fa1b3555f07b83b31da5cedba84b9c9e453b
muzammilpro <muzammilhusnain7@gmail.com>
2023-03-28 19:51:18 +0500

    New program added

---
+++ assignment no 07/Program no 06/javascript.js
@@ -0,0 +1,10 @@
+//Write a js program to check whether a year is leap year or not.
+
+var year =+ prompt("Enter a year:");
+
+if(year % 4 ==0){
+    console.log("This is leap year.");
+}
+else{
+    console.log("This is not a leap year.");
+}
