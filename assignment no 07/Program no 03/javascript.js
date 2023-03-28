c31a307f87d9bba275288f34fa66b2d34a8913ed
muzammilpro <muzammilhusnain7@gmail.com>
2023-03-28 11:19:00 +0500

    New Program added

---
+++ assignment no 07/Program no 03/javascript.js
@@ -0,0 +1,13 @@
+//Write a js program to check whether a number is negative, positive or zero.
+
+var num =+ prompt("Enter a Numer:");
+
+if( num > 0){
+    console.log("Number is Positive.");
+}
+else if( num < 0){
+    console.log("Number is Negative.");
+}
+else{
+    console.log("Number is Zero.");
+}
