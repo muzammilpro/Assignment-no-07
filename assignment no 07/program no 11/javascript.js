f196f8f4a3f759a40ac236a7a97963d92658653c
muzammilpro <muzammilhusnain7@gmail.com>
2023-03-30 05:36:58 +0500

    New program added

---
+++ assignment no 07/program no 11/javascript.js
@@ -0,0 +1,28 @@
+//Write a js program to input week number and print week day.
+
+var num =+ prompt("Enter the Number of a day(1-7):");
+
+if (num == 1) {
+    console.log("Monday");
+}
+ else if (num == 2) {
+    console.log("Tuesday");
+}
+else if (num == 3) {
+    console.log("Wednesday");
+}
+else if (num == 4) {
+    console.log("Thursday");
+}
+else if (num == 5) {
+    console.log("Friday");
+}
+else if (num == 6) {
+    console.log("Saturday");
+}
+else if (num == 7) {
+    console.log("Sunday");
+}
+else {
+    alert("Please enter correct Number.");
+}
