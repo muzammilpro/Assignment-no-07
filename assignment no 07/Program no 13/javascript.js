f1df3cd40381598708f3475ebd65536a6b0a6216
muzammilpro <muzammilhusnain7@gmail.com>
2023-04-01 23:38:59 +0500

    Assignment Complete

--- assignment no 07/Program no 13/javascript.js
+++ assignment no 07/Program no 13/javascript.js
@@ -1,1 +1,16 @@
 //Write a js program to count total number of notes in given amount.
+
+var amount = prompt("Enter amount: ");
+var notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+var count = [];
+
+for (var i = 0; i < notes.length; i++) {
+  var noteCount = Math.floor(amount / notes[i]);
+  count.push(noteCount);
+  amount -= noteCount * notes[i];
+}
+
+console.log("Total number of notes:");
+for (var i = 0; i < notes.length; i++) {
+  console.log(notes[i] + ": " + count[i]);
+}
